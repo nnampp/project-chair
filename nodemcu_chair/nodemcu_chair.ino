c2558bfa8cf8277b52c4412530518a18bf2e399e
KANYAPAK SODPO <62562661+nnampp@users.noreply.github.com>
2020-03-26 14:07:00 +0700

    delete some lines
    delete some lines

--- nodemcu_chair/nodemcu_chair.ino
+++ nodemcu_chair/nodemcu_chair.ino
@@ -39,7 +39,6 @@ while (NodeSerial.available() > 0) {
   str += ch;
   if(ch == '\n'){
   str.toCharArray(str2,31);
-  //Serial.println(str2);
   char *p = strtok(str2,",");
   left1 = atoi(p);
   p=strtok(NULL,",");
@@ -70,28 +69,10 @@ while (NodeSerial.available() > 0) {
   url += left3;
   url += "&left4=";
   url += left4;
-  //Serial.print("\nAnalog reading = ");
-  //Serial.println(fsrReading);     // the raw analog reading
   Serial.print("Requesting URL: ");
   Serial.println(url);
   client.print(String("GET ") + url + " HTTP/1.1\r\n" +
                "Host: " + host + "\r\n" +
                "Connection: close\r\n\r\n");
-  /*unsigned long timeout = millis();
-  while (client.available() == 0) {
-    if (millis() - timeout > 5000) {
-      Serial.println(">>> Client Timeout !");
-      client.stop();
-      return;
-    }
-  }*/
-  // Read all the lines of the reply from server and print them to Serial
-  /*while (client.available()) {
-    String line = client.readStringUntil('\r');
-    Serial.print(line);
-  } 
-  Serial.println();
-  Serial.println("closing connection");
-}      */
   delay(100);
 }
